1adcb0e02478ee5acb2f780e90b47f914050ed9c
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2025-11-21 08:14:19 -0800

    Create Emitter.h

---
+++ src/Emitter.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "ofMain.h"
+#include "Particle.h"
+
+class Emitter {
+public:
+    vector<Particle> particles;
+    glm::vec3 position;
+    float rate;        
+    glm::vec3 velocity;
+    bool oneShot = false;
+    float lastSpawnTime = 0;
+
+    void update();
+    void draw(ofShader &shader, ofCamera &cam);
+    void explode();    
+};
