70bc3465c334c767702cf744bfb97218772c4d3b
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2025-11-21 08:14:45 -0800

    Create Emitter.cpp

---
+++ src/Emitter.cpp
@@ -0,0 +1,49 @@
+#include "Emitter.h"
+#include "ofGraphics.h"
+
+void Emitter::update() {
+    float t = ofGetElapsedTimef();
+
+    //Continous exhause
+    if (!oneShot) {
+        if (t - lastSpawnTime > 1.0f / rate) {
+            particles.emplace_back(
+                position,
+                velocity + glm::vec3(ofRandom(-1,1), ofRandom(-1,1), ofRandom(-1,1))
+            );
+            lastSpawnTime = t;
+        }
+    }
+
+    //Remove dead particles
+    particles.erase(
+        remove_if(
+            particles.begin(),
+            particles.end(),
+            [&](Particle &p) { return p.age() > p.lifespan; }
+        ),
+        particles.end()
+    );
+
+    //Update particle positions
+    float dt = ofGetLastFrameTime();
+    for (auto &p : particles) {
+        p.pos += p.vel * dt;
+    }
+}
+
+void Emitter::draw(ofShader &shader, ofCamera &cam) {
+    shader.begin();
+
+    shader.setUniformMatrix4f("modelViewProjectionMatrix", cam.getModelViewProjectionMatrix());
+
+
+    glBegin(GL_POINTS);
+    for (auto &p : particles) {
+        shader.setUniform1f("age", p.age());
+        glVertex3f(p.pos.x, p.pos.y, p.pos.z);
+    }
+    glEnd();
+
+    shader.end();
+}
